dacc3e1cf4c31ce43ab31b2db4fc051cce35d5e6
Rasmus Selsmark <rasmus@unity3d.com>
2015-11-28 22:23:17 +0100

    Button to select character at beginning

--- Assets/Sample Scenes/Scripts/LevelController.cs
+++ Assets/Sample Scenes/Scripts/LevelController.cs
@@ -15,8 +15,8 @@ public class LevelController : MonoBehaviour
 	public GameObject StartMenuUI;
 	public GameObject LevelCompleteUI;
 
-	public GameObject[] Players;
-	public GameObject ActivePlayer;
+	public GameObject[] Characters;
+	public GameObject Player;
 	public Transform PlayerStartPosition;
 	public Transform Goal; // used to point camera to goal, so we can see where it is
 
@@ -25,30 +25,27 @@ public class LevelController : MonoBehaviour
 		UpdateUI ();
 
 		// only enable the current active player
-		foreach (var p in Players)
+		foreach (var character in Characters)
 		{
-			p.SetActive(p == ActivePlayer);
+			// set event handlers for our character
+			var player = character.GetComponent<CodingPiratesCharacter> ();
+			player.Died += PlayerDied;
+			player.LevelCompleted += PlayerLevelCompleted;
 		}
 
-		// set event handlers for our character
-		var player = ActivePlayer.GetComponent<CodingPiratesCharacter> ();
-		player.Died += PlayerDied;
-		player.LevelCompleted += PlayerLevelCompleted;
 		Camera.main.transform.position = new Vector3(Goal.position.x, Goal.position.y, Camera.main.transform.position.z);
-	}
-
-	void Update ()
-	{
-		if ((State == GameState.NotStarted || State == GameState.WonLevel) && Input.GetKeyDown (KeyCode.Space))
-		{
-			StartGame ();
-		}
 	}
 
 	private void StartGame()
 	{
-		Camera.main.GetComponent<FollowCamera> ().target = ActivePlayer.gameObject;
-		ActivePlayer.transform.position = PlayerStartPosition.position;
+		// only enable the current active player
+		foreach (var p in Characters)
+		{
+			p.SetActive (p == Player);
+		}
+
+		Camera.main.GetComponent<FollowCamera> ().target = Player.gameObject;
+		Player.transform.position = PlayerStartPosition.position;
 		State = GameState.Playing;
 		UpdateUI ();
 	}
@@ -61,6 +58,7 @@ public class LevelController : MonoBehaviour
 
 	void PlayerDied (object sender, System.EventArgs e)
 	{
+		(sender as MonoBehaviour).gameObject.SetActive (false);
 		State = GameState.NotStarted;
 		UpdateUI ();
 	}
@@ -72,22 +70,34 @@ public class LevelController : MonoBehaviour
 		case GameState.NotStarted:
 			StartMenuUI.SetActive (true);
 			LevelCompleteUI.SetActive (false);
-			ActivePlayer.gameObject.SetActive (false);
+			// Player.gameObject.SetActive (false);
 			break;
 
 		case GameState.Playing:
 			StartMenuUI.SetActive (false);
 			LevelCompleteUI.SetActive (false);
-			ActivePlayer.gameObject.SetActive (true);
-			ActivePlayer.GetComponent<CharacterMovement> ().IsPlaying = true;
+			Player.gameObject.SetActive (true);
+			Player.GetComponent<CharacterMovement> ().IsPlaying = true;
 			break;
 
 		case GameState.WonLevel:
 			StartMenuUI.SetActive (false);
 			LevelCompleteUI.SetActive (true);
-			ActivePlayer.gameObject.SetActive (true);
-			ActivePlayer.GetComponent<CharacterMovement> ().IsPlaying = false;
+			Player.gameObject.SetActive (true);
+			Player.GetComponent<CharacterMovement> ().IsPlaying = false;
 			break;
 		}
+	}
+
+	public void SelectPlayer(GameObject player)
+	{
+		this.Player = player;
+		StartGame ();
+	}
+
+	public void Restart()
+	{
+		State = GameState.NotStarted;
+		UpdateUI ();
 	}
 }
